6cd5982eee51bd76b3a7306cfa12998573eb4000
Spectacular2001 <ruchitadhore2001@gmail.com>
2025-01-31 00:46:47 +0530

    method overriding
    successfully override the class

---
+++ src/test/java/com/driver/test/main/java/com/driver/A.java
@@ -0,0 +1,7 @@
+package com.driver.test.main.java.com.driver;
+
+public class A {
+    public String meth() {
+        return "Invoking method from class A";
+    }
+}
---
+++ src/test/java/com/driver/test/main/java/com/driver/B.java
@@ -0,0 +1,9 @@
+package com.driver.test.main.java.com.driver;
+
+public class B extends A {
+    @Override
+    public String meth() {
+        return "Method is overridden in Extended class B";
+    }
+}
+
