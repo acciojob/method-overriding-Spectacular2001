adaf12772af3ee9c0697d3fb74683cfe7a3e36aa
Spectacular2001 <ruchitadhore2001@gmail.com>
2025-02-05 16:37:07 +0530

    update
    update

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -1,4 +1,17 @@
-ppackage com.driver.test; // or your test package
+package com.driver.test;  // Your package
 
-import com.driver.A;
+import com.driver.A;  // Import the necessary classes
 import com.driver.B;
+import org.junit.Test;
+
+public class TestCases {  // Start of the class
+
+    @Test  // Example annotation, if you're using JUnit
+    public void testMethod() {  // A sample test method
+        A a = new A();  // Object creation from class A
+        B b = new B();  // Object creation from class B
+
+        // Add assertions or logic for your test here
+    }
+}
+
